e85ce39fb2ad18c053992651fd6a916cb4c926b3
Travis-Gray2194 <travisogray1@gmail.com>
2018-04-14 18:18:37 -0400

    Initial commit

---
+++ src/main/java/me/uvahacks/Repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package me.uvahacks.Repositories;
+
+import me.uvahacks.Models.Role;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+    Role findByRole(String role);
+}
